80053f0a04fb180fc9ee448505b9bbbcfe0a2d92
sanskarIN <sanskarin@outlook.in>
2026-09-18 10:13:28 +0000

    fix(web): load NativeWind global stylesheet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -36,6 +36,7 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
+import "../global.css";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { ThemeProvider as CustomThemeProvider } from "@/lib/theme-provider";
