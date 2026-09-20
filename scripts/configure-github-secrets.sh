4b060698a9c1240698967e522c19a09941564900
sanskarIN <sanskarin@outlook.in>
2026-09-20 06:04:14 +0000

    chore(branding): update scripts-configure-github-secrets.sh

--- scripts/configure-github-secrets.sh
+++ scripts/configure-github-secrets.sh
@@ -3,8 +3,8 @@
 ################################################################################
 # MeTodo GitHub Secrets Configuration Script
 ################################################################################
-# (c) Copyright Sanskar Yadav. All rights reserved.
-# Made by Sanskar Yadav.
+# (c) Copyright Sanskar. All rights reserved.
+# Made by Sanskar.
 #
 # PURPOSE: Configure GitHub repository secrets for CI/CD automation
 #
