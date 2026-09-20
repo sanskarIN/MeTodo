330a93f3de54e2d43c9657ac2a12eb402fba1850
sanskarIN <sanskarin@outlook.in>
2026-09-20 06:04:12 +0000

    chore(branding): update app-(tabs)-_layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 /**
  * =============================================================================
- * (c) Copyright Sanskar Yadav. All rights reserved.
- * Made by Sanskar Yadav.
+ * (c) Copyright Sanskar. All rights reserved.
+ * Made by Sanskar.
  * =============================================================================
  *
  * FILE: app/(tabs)/_layout.tsx
