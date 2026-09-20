f1e0b3b8b453af5851fa23ea0806bf3df7d5e827
sanskarIN <sanskarin@outlook.in>
2026-09-20 06:04:14 +0000

    chore(branding): update scripts-build-android.sh

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -3,8 +3,8 @@
 ################################################################################
 # MeTodo Android Build Script
 # =============================================================================
-# (c) Copyright Sanskar Yadav. All rights reserved.
-# Made by Sanskar Yadav.
+# (c) Copyright Sanskar. All rights reserved.
+# Made by Sanskar.
 #
 # PURPOSE: Comprehensive Android build script for local development and CI/CD
 #
